23dc257b62278f5ec441e04bde8f9fb96616863f
zasch <zasch.sabene@gmail.com>
2018-03-18 10:07:45 +1100

    build a wall in front of perswon

--- hello.py
+++ hello.py
@@ -8,6 +8,10 @@ player_x, player_y, player_z  = mc.player.getTilePos()
 def bulldozer(x, y, z):
     mc.setBlocks(x - 27, y, z - 30, x + 30, y + 20, z + 30, 0)
 
-# Build things
-bulldozer(player_x, player_y, player_z)
+def buildWall(x, y, z):
+    mc.setBlocks(x, y, z, x + 20, y + 10, z, 5)
 
+# Build things
+# bulldozer(player_x, player_y, player_z)
+
+buildWall(player_x + 2, player_y, player_z + 2)
